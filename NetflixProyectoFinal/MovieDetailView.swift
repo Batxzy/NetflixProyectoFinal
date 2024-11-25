6e8dfa2d52d63b3f6926cf55062ecfcf6d5b7417
donguuu-u <lizbeth.aguayo3380@alumnos.udg.mx>
2024-11-25 04:50:08 -0600

    movie detail viwe

--- NetflixProyectoFinal/MovieDetailView.swift
+++ NetflixProyectoFinal/MovieDetailView.swift
@@ -7,44 +7,85 @@
 
 import SwiftUI
 
+
+
 struct MovieDetailView: View {
+    
+    let movie: MoviesMovies
+    
     var body: some View {
         
         ZStack {
-            LinearGradient(colors: [.black, .red], startPoint: .top, endPoint:.bottom)
+            LinearGradient(
+            stops: [
+            Gradient.Stop(color: Color(red: 0.49, green: 0.01, blue: 0.01), location: 0.00),
+            Gradient.Stop(color: .black, location: 0.70),
+            ],
+            startPoint: UnitPoint(x: 0.57, y: 1),
+            endPoint: UnitPoint(x: 0.57, y: 0.13)
+            )
+                .ignoresSafeArea()
             
-            VStack(alignment: .leading) {
-                Image("CyberpunkHorizontal")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(maxWidth: .infinity, maxHeight: 200)
-                    .clipped()
-                    .padding(.bottom)
+            VStack(alignment: .center,spacing: 5) {
+                ZStack {
+                   Image(movie.posterImageHorizontal)
+                       .resizable()
+                       .aspectRatio(contentMode: .fill)
+                       .frame(maxWidth: .infinity, maxHeight: 250)
+                       .clipped()
+                       .padding(.bottom)
+                   
+                   LinearGradient(
+                    gradient: Gradient(colors: [.clear, .black]),
+                       startPoint: .top,
+                       endPoint: .bottom
+                   )
+                   .frame(maxWidth: .infinity, maxHeight: 250)
+               }
                 
-                VStack{
-                    Text("Titulo")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.white)
-                    
-                    Text("Generes")
-                    ForEach(series.generos, id: \.self) { genero in
-                        Text(genero)
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 6)
-                            .background(Color.gray.opacity(0.3))
-                            .cornerRadius(16)
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 10){
+                        Text(movie.titulo)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.white)
+                        
+                        Text(movie.descripcion)
+                            .font(.callout)
+                        
+                        VStack(alignment: .leading, spacing: 15){
+                            Text("Generos:")
+                                .font(.system(size: 23, weight: .medium))
+                                .foregroundStyle(.white)
+                            
+                            VStack(alignment: .center, spacing: 10){
+                                ForEach(movie.generos, id: \.self) { genero in
+                                    Text(genero)
+                                        .font(.callout)
+                                        .fontWeight(.medium)
+                                        .padding(9)
+                                        .padding(.horizontal,5)
+                                        .background(.thinMaterial)
+                                        .cornerRadius(16)
+                                        .lineLimit(nil)
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                        }
+                        .frame(maxHeight:.infinity, alignment: .topLeading)
+                        
                         
                     }
+                    .frame(maxWidth: .infinity,alignment: .center)
+                    .padding(.horizontal,20)
                 }
             }
+            .frame(maxHeight: .infinity,alignment: .topLeading)
         }
         .frame(maxHeight: .infinity,alignment: .topLeading)
-        .ignoresSafeArea()
-        
     }
 }
 
 #Preview {
-    MovieDetailView()
+    MovieDetailView(movie:moviesCuatro )
 }
